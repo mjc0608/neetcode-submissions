9de37d06fee33a6a845a1bc85006dd82ce25a070
mjc0608 <i@jcma.me>
2026-09-09 19:31:14 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        def validLetter(c):
+            if c >= '0' and c <= '9':
+                return True
+            if c >= 'a' and c <= 'z':
+                return True
+            if c >= 'A' and c <= 'Z':
+                return True
+            return False
+
+        while i < j:
+            while not validLetter(s[i]) and i < j:
+                i += 1
+            while not validLetter(s[j]) and i < j:
+                j -= 1
+            print(s[i], s[j])
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
